bcd3ead83379881c728a3b9b2d52ee317b8cae46
cognitive-cake <nd.chernikov@gmail.com>
2018-07-24 16:06:25 +0300

    add some styles to SearchHeader

--- src/components/SearchHeader.js
+++ src/components/SearchHeader.js
@@ -10,13 +10,35 @@ const Button = styled.button`
   border: 2px solid palevioletred;
 `;
 
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  text-align: center;
+  background: linear-gradient(
+    151.09deg,
+    #00b0de 0.36%,
+    #01aedc 4.04%,
+    #02abdb 7.77%,
+    #02abdb 11.48%,
+    #02abdb 11.7%,
+    #196ebd 100%
+  );
+`;
+
+const SearchForm = styled.form``;
+
 class SearchHeader extends Component {
   render() {
     return (
-      <header>
+      <Header>
         Привет, я хедер!
-        <Button>Я кнопка!</Button>
-      </header>
+        <SearchForm>
+          <Button>Я кнопка!</Button>
+        </SearchForm>
+      </Header>
     );
   }
 }
